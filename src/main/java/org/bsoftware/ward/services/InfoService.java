b9491244be643add9f4811aeee83bb2dbbb6155f
Rudolf Barbu <Rudolf_Barbu@outlook.com>
2020-05-13 15:08:09 +0300

    Storage static labels implemented

--- src/main/java/org/bsoftware/ward/services/InfoService.java
+++ src/main/java/org/bsoftware/ward/services/InfoService.java
@@ -3,11 +3,10 @@ package org.bsoftware.ward.services;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import oshi.SystemInfo;
-import oshi.hardware.CentralProcessor;
-import oshi.hardware.ComputerSystem;
-import oshi.hardware.GlobalMemory;
-import oshi.hardware.PhysicalMemory;
+import oshi.hardware.*;
+
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @Service
@@ -21,9 +20,9 @@ public class InfoService
         String processorName = centralProcessor.getProcessorIdentifier().getName();
         if (processorName.contains("@"))
         {
-            processorName = processorName.substring(0, processorName.indexOf('@') - 1).trim();
+            processorName = processorName.substring(0, processorName.indexOf('@') - 1);
         }
-        infoBuffer.put("processorName", processorName);
+        infoBuffer.put("processorName", processorName.trim());
         int coreCount = centralProcessor.getLogicalProcessorCount();
         infoBuffer.put("coreCount", coreCount + ((coreCount > 1) ? " Cores" : " Core"));
         infoBuffer.put("maxClockSpeed", (Math.round((centralProcessor.getMaxFreq() / 1E+9) * 10.0) / 10.0) + " GHz");
@@ -46,12 +45,30 @@ public class InfoService
 
     private Map<String, String> getStorageInfo(Map<String, String> infoBuffer)
     {
-        System.out.println(systemInfo.getHardware().getDiskStores().get(0).getSize());
+        List<HWDiskStore> hwDiskStores = systemInfo.getHardware().getDiskStores();
+        String storageName = hwDiskStores.get(0).getModel();
+        if (storageName.contains("(Standard disk drives)"))
+        {
+            storageName = storageName.substring(0, storageName.indexOf("(Standard disk drives)") - 1);
+        }
+        infoBuffer.put("storageName", storageName.trim());
+        long totalStorage = hwDiskStores.stream().mapToLong(HWDiskStore::getSize).sum();
+        infoBuffer.put("totalStorage", Math.round(totalStorage / 1.074E+9) + " GiB Total");
+        int diskCount = systemInfo.getHardware().getDiskStores().size();
+        infoBuffer.put("diskCount", diskCount + ((diskCount > 1) ? " Disks" : " Disk"));
+        GlobalMemory globalMemory = systemInfo.getHardware().getMemory();
+        infoBuffer.put("swapAmount", globalMemory.getVirtualMemory().getSwapTotal() + " GiB Swap");
         return infoBuffer;
     }
 
+    @SuppressWarnings("IntegerDivisionInFloatingPointContext")
     private Map<String, String> getUptimeInfo(Map<String, String> infoBuffer)
     {
+        long uptimeInSeconds = systemInfo.getOperatingSystem().getSystemUptime();
+        infoBuffer.put("uptimeDays", String.valueOf(Math.round(Math.floor(uptimeInSeconds / 86400))));
+        infoBuffer.put("uptimeHours", String.valueOf(Math.round(Math.floor((uptimeInSeconds % 86400) / 3600))));
+        infoBuffer.put("uptimeMinutes", String.valueOf(Math.round(Math.floor((uptimeInSeconds / 60) % 60))));
+        infoBuffer.put("uptimeSeconds", String.valueOf(Math.round(Math.floor(uptimeInSeconds % 60))));
         return infoBuffer;
     }
 
