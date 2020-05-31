c4f1bfb617ceed73bb0287755fbf8961d81bcdcf
Rudolf Barbu <Rudolf_Barbu@outlook.com>
2020-05-31 20:51:56 +0300

    Added dynamic background

---
+++ src/main/resources/static/js/background.js
@@ -0,0 +1,13 @@
+function backgroundInitialization()
+{
+    VANTA.FOG
+    ({
+        el: "#background",
+        highlightColor: 0xcac7e8,
+        midtoneColor: 0xbbb7ed,
+        lowlightColor: 0xe4e3ef,
+        baseColor: 0xe4e3ef,
+        blurFactor: 0.40,
+        zoom: 1.50
+    });
+}
