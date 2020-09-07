22679342e96eb8e681ba8fbb2891d5dd0f095374
Rudolf_Barbu <Rudolf_Barbu@outlook.com>
2020-09-07 18:23:06 +0300

    Controller advices package moved to root

--- src/main/java/org/bsoftware/ward/controllers/SetupController.java
+++ src/main/java/org/bsoftware/ward/controllers/SetupController.java
@@ -6,7 +6,6 @@ import org.bsoftware.ward.services.implementation.SetupService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import javax.validation.Valid;
 
 /**
