de0756a08e4a2627c6724d70c6a57310ca61eece
Antony Leons <antony@leons.dev>
2024-01-05 16:04:26 +0000

    bump validation

--- src/main/java/dev/leons/ward/controllers/SetupController.java
+++ src/main/java/dev/leons/ward/controllers/SetupController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import javax.validation.Valid;
+import jakarta.validation.Valid;
 import java.io.IOException;
 
 /**
--- src/main/java/dev/leons/ward/dto/SetupDto.java
+++ src/main/java/dev/leons/ward/dto/SetupDto.java
@@ -1,14 +1,13 @@
 package dev.leons.ward.dto;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
-
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
-import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
-import javax.validation.constraints.Size;
 
 /**
  * SetupDto is a values container for setup data
@@ -24,8 +23,7 @@ public class SetupDto
      * Server name Field
      */
     @NotNull
-    @NotEmpty
-    @Size(min = 1, max = 10)
+    @Size(min = 0, max = 10)
     private String serverName;
 
     /**
